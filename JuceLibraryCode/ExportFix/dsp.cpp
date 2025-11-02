930fa6195ff38978a85959e49598197afaa6a672
無常 <3142324836@qq.com>
2025-11-02 20:36:52 +0800

    Fixed symbols export

--- JuceLibraryCode/ExportFix/dsp.cpp
+++ JuceLibraryCode/ExportFix/dsp.cpp
@@ -1,44 +1,25 @@
 #include <JuceHeader.h>
 
 template class JUCE_API juce::dsp::Bias<float>;
-template class JUCE_API juce::dsp::Chorus<float>;
-template class JUCE_API juce::dsp::Compressor<float>;
 template class JUCE_API juce::dsp::Gain<float>;
-template class JUCE_API juce::dsp::LadderFilter<float>;
-template class JUCE_API juce::dsp::Limiter<float>;
-template class JUCE_API juce::dsp::NoiseGate<float>;
 template class JUCE_API juce::dsp::Oscillator<float>;
-template class JUCE_API juce::dsp::Phaser<float>;
 template class JUCE_API juce::dsp::WaveShaper<float>;
 
-template class JUCE_API juce::dsp::BallisticsFilter<float>;
-template class JUCE_API juce::dsp::DelayLine<float>;
-template class JUCE_API juce::dsp::DryWetMixer<float>;
 template class JUCE_API juce::dsp::FIR::Coefficients<float>;
 template class JUCE_API juce::dsp::FIR::Filter<float>;
-template class JUCE_API juce::dsp::FirstOrderTPTFilter<float>;
 template class JUCE_API juce::dsp::IIR::ArrayCoefficients<float>;
 template class JUCE_API juce::dsp::IIR::Coefficients<float>;
 template class JUCE_API juce::dsp::IIR::Filter<float>;
-template class JUCE_API juce::dsp::LinkwitzRileyFilter<float>;
-template class JUCE_API juce::dsp::Oversampling<float>;
-template class JUCE_API juce::dsp::Panner<float>;
 template class JUCE_API juce::dsp::ProcessContextReplacing<float>;
 template class JUCE_API juce::dsp::ProcessContextNonReplacing<float>;
 //template class JUCE_API juce::dsp::ProcessorChain<float>;
 //template class JUCE_API juce::dsp::ProcessorWrapper<float>;
 template class JUCE_API juce::dsp::StateVariableFilter::Parameters<float>;
 template class JUCE_API juce::dsp::StateVariableFilter::Filter<float>;
-template class JUCE_API juce::dsp::StateVariableTPTFilter<float>;
 
 template class JUCE_API juce::dsp::LogRampedValue<float>;
-template class JUCE_API juce::dsp::LookupTable<float>;
-template class JUCE_API juce::dsp::LookupTableTransform<float>;
-template class JUCE_API juce::dsp::Matrix<float>;
 template class JUCE_API juce::dsp::Phase<float>;
 template class JUCE_API juce::dsp::Polynomial<float>;
-
-template class JUCE_API juce::dsp::WindowingFunction<float>;
 
 template class JUCE_API juce::dsp::FilterDesign<float>;
 
